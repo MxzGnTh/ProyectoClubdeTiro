f1d20d589e8ddf50e0bac0b997320de7c72fb9b0
MxzGnTh <mixzio.gonzalez@gmail.com>
2022-04-26 22:42:37 -0400

    perfildeusuarioprimeraparte

--- routes/front.route.js
+++ routes/front.route.js
@@ -2,36 +2,41 @@ const {Router} = require('express');
 const router = Router();
 //=====================================================//
 
-// Ruta a Inicio
+//Inicio
 router.get('/', (req, res)=>{
 res.render('inicio.hbs')
 })
 
-//Ruta a REGISTRAR
+//REGISTRAR
 router.get('/registrar',(req, res)=>{
     res.render('registrar.hbs')
 })
 
-//Ruta INICIAR SESIÓN
+//INICIAR SESIÓN
 router.get('/iniciarsesion', (req, res)=>{
     res.render('iniciarsesion.hbs')
 })
 
-//Ruta a ADMIN
+// ADMIN
 router.get('/Admin', (req, res)=>{
     res.render('Admin.hbs')
 })
 
-//Ruta a USUARIO
+//USUARIO
 router.get('/usuario',(req, res)=>{
     res.render('usuario.hbs')
 })
 
-//ruta a INICIAR ADMIN
+// INICIAR ADMIN
 router.get('/iniciarAdmin', (req, res)=>{
     res.render('iniciarAdmin.hbs')
 })
 
+
+// PERFIL USUARIO
+router.get('/perfilusuario', (req, res)=>{
+    res.render('perfilusuario.hbs')
+})
 
 
 module.exports = router
